5ce157853e15d90f3712605cc1bd00d4dcbba000
Jasper Chen <jasperdyb@gmail.com>
2020-06-28 21:27:55 +0800

    React with pug init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
+const App = () => pug`
+  div(className="app")
+    header(className="App-header")
+      img(src=logo className="App-logo" alt="logo")
+      p
+      |Edit
+      code src/App.js
+      |and save to reload.
+      a(className="App-link"
           href="https://reactjs.org"
           target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+          rel="noopener noreferrer") Learn React
+  `;
 
 export default App;
