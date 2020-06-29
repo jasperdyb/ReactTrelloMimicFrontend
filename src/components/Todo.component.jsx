758d90034954001b11c954115a536652991e301d
Jasper Chen <jasperdyb@gmail.com>
2020-06-29 18:16:19 +0800

    functioned drag n drop , but not totally correct action

--- src/components/Todo.component.jsx
+++ src/components/Todo.component.jsx
@@ -1,12 +1,47 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { ItemTypes } from "../dnd/constants.js";
+import { useDrag, useDrop } from "react-dnd";
 
-export default function Todo({ todo }) {
+export default function Todo({ todo, index, handleMoveTodo }) {
+  const [{ isDragging }, drag] = useDrag({
+    item: { type: ItemTypes.TODO, index },
+    collect: (monitor) => ({
+      isDragging: !!monitor.isDragging(),
+    }),
+  });
+
+  const [{ isOver }, drop] = useDrop({
+    accept: ItemTypes.TODO,
+    drop: (todo) => handleMoveTodo(todo, index),
+    collect: (monitor) => ({
+      isOver: !!monitor.isOver(),
+    }),
+  });
+
   return pug`
-    a.btn.d-flex.todo-item(href="#") #{todo.name}
+    div(ref=drag
+      style={
+        opacity: isDragging ? 0.5 : 1,
+        fontSize: 25,
+        fontWeight: 'bold',
+        cursor: 'move',
+      })
+      div(ref=drop
+      style={
+        position: 'relative',
+        width: '100%',
+        height: '100%',
+      })
+        a.btn.d-flex.todo-item(href="#") #{todo.name}
+        
+      if isOver 
+        a.btn.d-flex.todo-item(href="#") #{todo.name}
   `;
 }
 
 Todo.propTypes = {
   todo: PropTypes.object,
+  index: PropTypes.number,
+  handleMoveTodo: PropTypes.func,
 };
--- src/components/TodoList.component.jsx
+++ src/components/TodoList.component.jsx
@@ -1,11 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 import Todo from "./Todo.component";
+// import { ItemTypes } from "../dnd/constants.js";
+// import { useDrop } from "react-dnd";
 
-export default function TodoList({ todoItems }) {
+export default function TodoList(props) {
+  const [todoItems, setTodoItems] = useState(props.todoItems);
+
+  const handleMoveTodo = (fromTodo, toIndex) => {
+    const fromIndex = fromTodo.index;
+    const movedTodo = todoItems.splice(fromIndex, 1);
+    const tails = todoItems.splice(toIndex + 1);
+    const newTodos = todoItems.concat(movedTodo).concat(tails);
+    setTodoItems(newTodos);
+  };
+
   const Todos = todoItems.map((todo, index) => {
+    const propsToTodo = { todo, index, handleMoveTodo };
     return pug`
-      Todo(key=todo.name todo=todo) 
+      Todo(key=index ...propsToTodo ) 
     `;
   });
 
