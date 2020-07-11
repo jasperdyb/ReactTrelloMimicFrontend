9d9b78bffdc9a483c2ca137d4724731355d3153b
Jasper Chen <jasperdyb@gmail.com>
2020-07-12 00:27:40 +0800

    serve build

---
+++ build/precache-manifest.c33b6eb9126717fcc7a46bd9bfbeaa93.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fed472b81a753678033378e591f40879",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b9e618ba28357a23dd08",
+    "url": "/static/css/main.311816df.chunk.css"
+  },
+  {
+    "revision": "d3db188f8a4c2145fa2b",
+    "url": "/static/js/2.8da99029.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/static/js/2.8da99029.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9e618ba28357a23dd08",
+    "url": "/static/js/main.1ea815cc.chunk.js"
+  },
+  {
+    "revision": "559ff28579f192bdf8b9",
+    "url": "/static/js/runtime-main.98bec8e4.js"
+  }
+]);
